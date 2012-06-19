9e69ffc605d17d5dad62ac6a5e6cf3c77720b54b
vorbeth <vorbeth@gmail.com>
2012-06-19 19:53:50 +0100

    pyramid: add thermald & spdecision
    Change-Id: Ica6fe0b2501e2dff733dc9bd7925a0da2feb5491

--- pyramid-vendor-blobs.mk
+++ pyramid-vendor-blobs.mk
@@ -28,9 +28,11 @@ PRODUCT_COPY_FILES += \
   vendor/htc/pyramid/proprietary/bin/htcbatt:system/bin/htcbatt \
   vendor/htc/pyramid/proprietary/bin/ipd:system/bin/ipd \
   vendor/htc/pyramid/proprietary/bin/load-modem.sh:system/bin/load-modem.sh \
+  vendor/htc/pyramid/proprietary/bin/mpdecision:system/bin/mpdecision \
   vendor/htc/pyramid/proprietary/bin/netmgrd:system/bin/netmgrd \
   vendor/htc/pyramid/proprietary/bin/netsharing:system/bin/netsharing \
   vendor/htc/pyramid/proprietary/bin/rmt_storage:system/bin/rmt_storage \
+  vendor/htc/pyramid/proprietary/bin/thermald:system/bin/thermald \
   vendor/htc/pyramid/proprietary/bin/qmuxd:system/bin/qmuxd \
   vendor/htc/pyramid/proprietary/lib/libacdbloader.so:system/lib/libacdbloader.so \
   vendor/htc/pyramid/proprietary/lib/libacdbmapper.so:system/lib/libacdbmapper.so \
